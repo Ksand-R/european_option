33abb868ea8f82628f8d066a5708630511958c39
x-ndr <al.romanov2012@yandex.ru>
2018-02-27 02:19:55 +0300

    Aded full automation

--- main_proj/linux_script.py
+++ main_proj/linux_script.py
@@ -3,7 +3,9 @@ import sys, datetime, os, time
 
 now = datetime.datetime.now()
 
-N       = [1000000, 2000000, 3000000, 4000000]
+#TODO: run script with file.out from argv
+file 	= (sys.argv[1])
+N       = [100000000 ,268435456]
 count   = 7
 num_seq_ver = 7
 sec_times = []
@@ -11,6 +13,12 @@ par_times = []
 
 par_ver = {0:7, 1:8}
 threads = {0:1, 1:2, 2:4, 3:8, 4:16}
+affinity = {0: '',
+			1: 'GOMP_CPU_AFFINITY="0 1" OMP_NUM_THREADS=2 ',
+			2: 'GOMP_CPU_AFFINITY="0 1 2 3" OMP_NUM_THREADS=4 ',
+			3: 'GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7" OMP_NUM_THREADS=8 ',
+			4: 'GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15" OMP_NUM_THREADS=16 '
+			}
 abs_path = os.getcwd()
 
 dt = now.strftime("%d-%m-%Y-%H:%M:%S")
@@ -28,21 +36,21 @@ def min_res(version, name):
     min_time = 10000
     
     for i in range(count):
-    
         time = float(one_res(version, name))
         if time < min_time:
             min_time = time
     return min_time
 
 def get_times(_N):
-    for version in range(7):
-        name = "srun -n 1 -p all -t 20 " + os.path.join(os.getcwd(), 'a.out ') + \
+    for version in range(num_seq_ver):
+        name = "srun -n 1 -p all -t 30 " + os.path.join(os.getcwd(), str(file)) + " " +\
             str(version) +" "+ str(_N) + " " + str(1)
         sec_times.append((min_res(version, name))) 
 
     for version in par_ver:
         for thr in threads:
-                    name = "srun -n 1 -p all -t 10 " + os.path.join(os.getcwd(), 'a.out ') + \
+                    name = affinity[thr] + "srun -n 1 -p all -t 20 " + \
+                        os.path.join(os.getcwd(), str(file)) + " " +\
                         str(par_ver[version]) +" "+ str(_N) + " " + str(threads[thr])
                     par_times.append((min_res(par_ver[version], name))) 
 
@@ -54,7 +62,8 @@ def write_log(N_):
     os.chdir(dt)
     _iter = 0
     with open('!summary_' + str(N_) + '.log', 'a') as f:
-	    print(sec_times)
+	    #print(sec_times)
+	    print(datetime.datetime.now())
 	    for i in sec_times:   
 	    	info = "%s %s %s %s %s %s %s %s" % ("version ", str(_iter), "; amo of opts ", \
 	    		str(N_), "; num tr ", str(1), "; counts ", str(count))     
@@ -62,21 +71,24 @@ def write_log(N_):
 	        _iter += 1
 
 	    _iter = 0
-	    print(par_times)
-	    for i in par_times:   
+	    #print(par_times)
+	    f.write(80 * "-" + "\n") 
+	    for i in par_times:  
+
 	        info = "%s %s %s %s %s %s %s %s" % \
 	            ("version ", str((_iter // 5) + 7), "; amo of opts ", str(N_), "; num tr ", \
 	            str(threads[_iter % 5]), "; counts ", str(count))
-	        f.write(info + "; min time " +  str(i) + " seconds" + "\n")
+	        f.write(info + "; min time " +  str(i) + " seconds" + "\n" )
+	        #f.write(str(affinity[_iter % 5]) + "\n")
 	        _iter += 1    
+	    f.write(80 * "-") 
     del sec_times[:]
     del par_times[:]
-
 
 if __name__ == '__main__':
 	for i in N:
 	    get_times(i)
 	    write_log(i)
 	    os.chdir(abs_path)
-        print(sec_times)
-        print(par_times)
+        #print(sec_times)
+        #print(par_times)
