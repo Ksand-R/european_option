08bb7cfeb2596d26689c8f6529c56293995182ea
x-ndr <al.romanov2012@yandex.ru>
2018-04-16 13:09:55 +0300

    Added group of projects for Call + Put options

---
+++ Call+put/V7.cpp
@@ -0,0 +1,25 @@
+//self_counting_invsqrt
+#include "Header.h"
+
+void _V7(float* pT, float* pK, float* pS0, float* pC, float *pP)
+{
+	int		i;
+	float	d1, d2, erf1, erf2;
+
+#pragma simd
+	for (i = 0; i < N; i++)
+	{
+		float tmp = sig2 * pT[i];
+		vsInvSqrt(1, &tmp, &invf);
+		d1 = (logf(pS0[i] / pK[i]) + (r + sig * sig * 0.5f)
+			* pT[i]) / (sig * invf);
+		d2 = (logf(pS0[i] / pK[i]) + (r - sig * sig * 0.5f)
+			* pT[i]) / (sig * invf);
+
+		erf1 = 0.5f + 0.5f * erff(d1 * invsqrt2);
+		erf2 = 0.5f + 0.5f * erff(d2 * invsqrt2);
+
+		pC[i] = pS0[i] * erf1 - pK[i] * expf((-1.0f) * r * pT[i]) * erf2;
+		pP[i] = pK[i] * expf((-1.0) * r * pT[i]) * (1 - erf2) - pS0[i] * (1 - erf1);
+	}
+}
---
+++ Call+put/linux_script.py
@@ -0,0 +1,100 @@
+from subprocess import Popen, PIPE
+import sys, datetime, os, time
+
+now = datetime.datetime.now()
+
+file 	= (sys.argv[1])
+N       = [10000 ,26843]
+count   = 7
+sec_times = []
+num_seq_ver = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:9}
+len_seq = 8
+par_times = []
+
+par_ver = {0:7, 1:8}
+threads = {0:1, 1:2, 2:4, 3:8, 4:16}
+affinity = {0: '',
+			1: 'GOMP_CPU_AFFINITY="0 1" OMP_NUM_THREADS=2 ',
+			2: 'GOMP_CPU_AFFINITY="0 1 2 3" OMP_NUM_THREADS=4 ',
+			3: 'GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7" OMP_NUM_THREADS=8 ',
+			4: 'GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15" OMP_NUM_THREADS=16 '
+			}
+abs_path = os.getcwd()
+
+dt = now.strftime("%d-%m-%Y-%H:%M:%S")
+
+def one_res(version, name):
+   
+    proc = Popen(name, shell=True, stdout=PIPE, stderr=PIPE)
+    proc.wait()    
+    res = proc.communicate()  
+    if proc.returncode:
+       return res[1]
+    return res[0]
+
+def min_res(version, name):
+    min_time = 10000
+    
+    for i in range(count):
+        time = float(one_res(version, name))
+        if time < min_time:
+            min_time = time
+    return min_time
+
+def get_times(_N):
+    for version in range(8):
+        name = "srun -n 1 -p all -t 30 " + os.path.join(os.getcwd(), str(file)) + " " +\
+            str(num_seq_ver[version]) +" "+ str(_N) + " " + str(1)
+        print(name)
+        print(num_seq_ver[version])
+        sec_times.append((min_res(float(num_seq_ver[version]), name))) 
+
+    # name = "srun -n 1 -p all -t 30 " + os.path.join(os.getcwd(), str(file)) + " " +\
+    #     str(9) +" "+ str(_N) + " " + str(1)
+    # sec_times.append((min_res(version, name)))
+
+    for version in par_ver:
+        for thr in threads:
+                    name = affinity[thr] + "srun -n 1 -p all -t 20 " + \
+                        os.path.join(os.getcwd(), str(file)) + " " +\
+                        str(par_ver[version]) +" "+ str(_N) + " " + str(threads[thr])
+                    par_times.append((min_res(par_ver[version], name))) 
+
+
+def write_log(N_):
+    os.chdir("logs")
+    if not os.path.exists(dt) and not os.path.isfile(dt):
+        os.mkdir(dt)
+    os.chdir(dt)
+    _iter = 0
+    with open('!summary_' + str(N_) + '.log', 'a') as f:
+	    #print(sec_times)
+	    print(datetime.datetime.now())
+	    for i in sec_times:   
+	    	info = "%s %s %s %s %s %s %s %s" % ("version ", str(_iter), "; amo of opts ", \
+	    		str(N_), "; num tr ", str(1), "; counts ", str(count))     
+	        f.write(info + "; min time " +  str(i) + " seconds" +"\n")
+	        _iter += 1
+
+	    _iter = 0
+	    #print(par_times)
+	    f.write(80 * "-" + "\n") 
+	    for i in par_times:  
+
+	        info = "%s %s %s %s %s %s %s %s" % \
+	            ("version ", str((_iter // 5) + 7), "; amo of opts ", str(N_), "; num tr ", \
+	            str(threads[_iter % 5]), "; counts ", str(count))
+	        f.write(info + "; min time " +  str(i) + " seconds" + "\n" )
+	        #f.write(str(affinity[_iter % 5]) + "\n")
+	        _iter += 1    
+	    f.write(80 * "-") 
+    del sec_times[:]
+    del par_times[:]
+
+if __name__ == '__main__':
+	for i in N:
+	    get_times(i)
+	    write_log(i)
+	    os.chdir(abs_path)
+        #print(sec_times)
+        #print(par_times)
